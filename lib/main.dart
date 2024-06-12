bea3e69c45f45d47feac87ba0de3f05f5239ce5b
Montag <103876121+MontagMakes@users.noreply.github.com>
2024-06-12 16:54:50 +0500

    wasm try, changed splash screen color to white from index.html

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Arsalan K. Nizamani',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
